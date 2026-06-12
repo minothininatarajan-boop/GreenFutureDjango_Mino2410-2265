4d574ce640a579ef89c18996608a95a75287cbe6
Minothini <minothininatarjan@gmail.com>
2026-06-12 19:59:21 +0800

    First commit - Green Future Django project

---
+++ volunteer/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Volunteer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    interest = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
---
+++ volunteer/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from .models import Volunteer
+
+
+def home(request):
+    return render(request, 'volunteer/home.html')
+
+
+def about(request):
+    return render(request, 'volunteer/about.html')
+
+
+def register(request):
+
+    if request.method == "POST":
+
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        interest = request.POST['interest']
+
+        Volunteer.objects.create(
+            name=name,
+            email=email,
+            phone=phone,
+            interest=interest
+        )
+
+        return render(request, 'volunteer/register.html',
+                      {'message': 'Registration Successful!'})
+
+    return render(request, 'volunteer/register.html')
