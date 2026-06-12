0d265d2d83dab3b99d32cd98e52c66567078eaef
Minothini <minothininatarjan@gmail.com>
2026-06-12 21:29:12 +0800

    Create admin user for deployment

--- volunteer/urls.py
+++ volunteer/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('register/', views.register, name='register'),
+    path('', views.home),
+    path('register/', views.register),
+    path('about/', views.about),
+    path('create-admin/', views.create_admin),
 ]
+
--- volunteer/views.py
+++ volunteer/views.py
@@ -30,3 +30,21 @@ def register(request):
                       {'message': 'Registration Successful!'})
 
     return render(request, 'volunteer/register.html')
+
+   from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+
+def create_admin(request):
+
+    if not User.objects.filter(username="admin").exists():
+
+        User.objects.create_superuser(
+            username="admin",
+            email="admin@gmail.com",
+            password="GreenFuture123"
+        )
+
+        return HttpResponse("Admin created successfully")
+
+    return HttpResponse("Admin already exists") 
